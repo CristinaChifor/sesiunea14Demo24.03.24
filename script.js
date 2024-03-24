ac60ca714ae07f7879e9e2783ef36633f1117df6
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-24 14:17:35 +0200

    Manipulate the DOM (WIP)

--- script.js
+++ script.js
@@ -43,4 +43,75 @@ console.log(cardParagraph);
 
 const allTodos = document.querySelectorAll('#card > ol > .todo-item');
 console.log(allTodos);
+console.log(allTodos[0].textContent);
+console.log(allTodos[0].innerText);
+
+// Manipulate the DOM
+
+// 1. textContent și innerHTML: 
+//Aceste proprietăți permit accesarea și modificarea conținutului text al unui element. textContent returnează sau setează doar textul, în timp ce innerHTML permite lucrul cu conținut HTML.
+
+const p = document.querySelector('p');
+p.textContent = 'This was added with JS';
+
+const footer = document.querySelector('footer');
+const author = 'John Doe';
+footer.innerHTML = `<strong>Author:</strong><span>${author}</span>`;
+
+const posts = [
+    {
+        author: 'Nick',
+        todos: ['do 1', 'do 2'],
+        comment: 'My comment',
+        title: 'card 1'
+    },
+    {
+        author: 'Nick',
+        todos: ['do 1', 'do 2'],
+        comment: 'My comment',
+        title: 'card 2',
+    },
+    {
+        author: 'Nick',
+        todos: ['do 1', 'do 2'],
+        comment: 'My comment',
+        title: 'card 3'
+    }
+];
+
+const body = document.querySelector('body');
+let htmlContent = '';
+for (const post of posts) {
+    const card = `<article class="card">
+    <p>${post.title}</p>
+    
+    <ol>
+        
+        ${post.todos.map((el) => `<li class="todo-item">` + el +`</li>`)}
+    </ol>
+    
+    <label for="comment">Comment</label>
+    <input type="text" id="comment" name="comment" value ="${post.comment}>
+    <footer></footer>
+    </article>`
+
+    htmlContent += card;
+}
+
+body.innerHTML = htmlContent;
+//body.textContent = htmlContent;
+
+// innerText vs textContent
+const firstp = document.querySelector('p');
+console.log(firstp);
+console.log(firstp.textContent);
+firstp.textContent = 'ast';
+console.log(firstp.innerText);
+firstp.innerText = 'asdg';
+console.log(firstp.innerText);
+console.log(firstp.textContent);
+
+// 2. getAttribute și setAttribute: 
+// Aceste metode permit accesarea și modificarea atributelor unui element.
+
 
